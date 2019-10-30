dc06ace55b91e949a71c8622693ba79ab99fe103
Hugo CANAL <contact@piafthebest.fr>
2019-10-30 16:48:54 +0100

    Adding SQL file

---
+++ convoymanager.sql
@@ -0,0 +1,118 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mer. 30 oct. 2019 à 15:45
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `convoymanager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `convoy`
+--
+
+DROP TABLE IF EXISTS `convoy`;
+CREATE TABLE IF NOT EXISTS `convoy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL,
+  `origin` varchar(255) NOT NULL,
+  `destination` varchar(255) NOT NULL,
+  `type` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `convoy`
+--
+
+INSERT INTO `convoy` (`id`, `name`, `date`, `origin`, `destination`, `type`) VALUES
+(1, 'Convoi inter entreprise LFA', '2019-07-17 06:25:31', 'Paris', 'Palerme', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `convoy_part`
+--
+
+DROP TABLE IF EXISTS `convoy_part`;
+CREATE TABLE IF NOT EXISTS `convoy_part` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `user_convoy` int(11) NOT NULL,
+  `date` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `convoy_part`
+--
+
+INSERT INTO `convoy_part` (`id`, `name`, `user_convoy`, `date`) VALUES
+(1, 'piaf', 1, '2019-07-04 04:09:18'),
+(2, 'flash', 1, '2019-07-26 09:16:17');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `links`
+--
+
+DROP TABLE IF EXISTS `links`;
+CREATE TABLE IF NOT EXISTS `links` (
+  `id` int(1) NOT NULL DEFAULT '1',
+  `trucksbook` varchar(255) NOT NULL DEFAULT 'https://trucksbook.eu',
+  `discord` varchar(255) NOT NULL DEFAULT 'https://discordapp.com',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `links`
+--
+
+INSERT INTO `links` (`id`, `trucksbook`, `discord`) VALUES
+(1, 'https://trucksbook.eu', 'https://discordapp.com');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `user`, `pass`) VALUES
+(1, 'piafthebest', '$2y$10$.fr7Nai7wIkC6ckLpFqFluaF7vXerbpcCB24.YdiH1I9UXqY0Xu1i'),
+(3, 'admin', '$2y$10$D7DjoslSfPgS3Ab/q0cg4e2LEUgoWXjB5Tm2VLcAEwUpjdpe.rIRy');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
